d50fd08b6ed962ecc2ed1e664163353504fe0d11
kindheart-is-good <oneofmycontacts5@gmail.com>
2022-11-19 12:00:04 +0300

    Create MainPage component

---
+++ src/components/MainPage/MainPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './MainPage.module.css';
+
+const MainPage = (props) => {
+
+    return (
+        <div>
+        </div>
+    )
+}
+
+export default MainPage;
