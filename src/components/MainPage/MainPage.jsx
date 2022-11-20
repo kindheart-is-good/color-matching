4b5694e5e790aa910b4f5b57b57d9be81a19c4d1
kindheart-is-good <oneofmycontacts5@gmail.com>
2022-11-20 23:24:39 +0300

    Style: Create CSS Grid inside MainPage component

--- src/components/MainPage/MainPage.jsx
+++ src/components/MainPage/MainPage.jsx
@@ -4,7 +4,12 @@ import styles from './MainPage.module.css';
 const MainPage = (props) => {
 
     return (
-        <div>
+        <div className={styles.wrapper}>
+            <div className={styles.top}>top</div>
+            <div className={styles.first}>first</div>
+            <div className={styles.second}>second</div>
+            <div className={styles.third}>third</div>
+            <div className={styles.bot}>bot</div>
         </div>
     )
 }
